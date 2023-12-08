3ed48d41afecf716c2c853d0e9236d6c2ac50795
DILPREET1910 <dsahney1910@gmail.com>
2023-12-08 13:58:38 +0530

    telescope functions

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,2 +1,5 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n','<leader>ff',builtin.find_files,{}) -- find files in project files
+vim.keymap.set('n','<leader><leader>',builtin.oldfiles,{}) -- previously opened files in any project
+vim.keymap.set('n','<leader>fg',builtin.live_grep,{}) -- find in file (requires ripgrep to be installed on system)
+vim.keymap.set('n','<leader>fh',builtin.help_tags,{}) -- help tags
