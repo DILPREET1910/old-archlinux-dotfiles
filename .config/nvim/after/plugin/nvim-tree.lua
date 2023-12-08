89cf84887289c8f73170b8b216d2a1e90a4b09d4
DILPREET1910 <dsahney1910@gmail.com>
2023-12-08 18:05:38 +0530

    nvim tree close after opening file

--- .config/nvim/after/plugin/nvim-tree.lua
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -1,6 +1,12 @@
 vim.g.loaded_netq=1
 vim.g.loaded_netrwPlugin=1
 
-require('nvim-tree').setup()
+require('nvim-tree').setup({
+  actions = {
+    open_file = {
+      quit_on_open = true, -- quite after opening file
+    }
+  }
+})
 
 vim.keymap.set('n','<C-n>',':NvimTreeFindFileToggle<CR>')
