94cbe742e3fa2e606acf7703c4b1e841facc8f7a
DILPREET1910 <dsahney1910@gmail.com>
2024-01-15 14:06:51 +0530

    open diagnostics on floating window

--- .config/nvim/after/plugin/lspconfig.lua
+++ .config/nvim/after/plugin/lspconfig.lua
@@ -1,74 +1,75 @@
-require('mason').setup()
-require('mason-lspconfig').setup({
-	ensure_installed = {'lua_ls'}
+require("mason").setup()
+require("mason-lspconfig").setup({
+	ensure_installed = { "lua_ls" },
 })
 
 --#########################################
 -- Define Functions and Variables here
 --#########################################
-local on_attach = function(_,_)
-	vim.keymap.set('n','<leader>rn',vim.lsp.buf.rename,{}) -- rename variables in current buffer
-	vim.keymap.set('n','<leader>ca',vim.lsp.buf.code_action, {}) -- code action
-	vim.keymap.set('n','gd',vim.lsp.buf.definition,{}) -- get references in current buffer
-	vim.keymap.set('n','gr',require('telescope.builtin').lsp_references,{}) -- get references using telescope in current buffer
-	vim.keymap.set('n','K',vim.lsp.buf.hover,{}) -- documentation
+local on_attach = function(_, _)
+	vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {}) -- rename variables in current buffer
+	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {}) -- code action
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, {}) -- get references in current buffer
+	vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, {}) -- get references using telescope in current buffer
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, {}) -- documentation
+	vim.keymap.set("n", "<leader>e", ":lua vim.diagnostic.open_float()<CR>", {}) -- documentation
 end
 
-local capabilites = require('cmp_nvim_lsp').default_capabilities()
+local capabilites = require("cmp_nvim_lsp").default_capabilities()
 
 --#########################################
 -- Require LSPs here
 --#########################################
 -- lua
-require('lspconfig').lua_ls.setup{
+require("lspconfig").lua_ls.setup({
 	on_attach = on_attach,
-  capabilities = capabilites,
-	settings={
+	capabilities = capabilites,
+	settings = {
 		Lua = {
 			diagnostics = {
-				globals = {'vim'}
-			}
-		}
-	}
-}
+				globals = { "vim" },
+			},
+		},
+	},
+})
 
 -- dart flutter [inbuild lsp that comes with dart (no need to install it with mason)]
-require('lspconfig').dartls.setup{
+require("lspconfig").dartls.setup({
 	on_attach = on_attach,
-  capabilities = capabilites,
-	cmd={'/opt/flutter/bin/dart','language-server','--protocol=lsp'},
-}
+	capabilities = capabilites,
+	cmd = { "/opt/flutter/bin/dart", "language-server", "--protocol=lsp" },
+})
 
 -- json lsp (jsonls)
-require('lspconfig').jsonls.setup{
+require("lspconfig").jsonls.setup({
 	on_attach = on_attach,
-  capabilities = capabilites,
-}
+	capabilities = capabilites,
+})
 
 -- java lsp (jdtls)
-require('lspconfig').jdtls.setup{
+require("lspconfig").jdtls.setup({
 	on_attach = on_attach,
-  capabilities = capabilites,
-}
+	capabilities = capabilites,
+})
 
 -- html
-require('lspconfig').html.setup{
+require("lspconfig").html.setup({
 	on_attach = on_attach,
-  capabilities = capabilites,
-}
+	capabilities = capabilites,
+})
 
 -- tailwind
-require('lspconfig').tailwindcss.setup{
+require("lspconfig").tailwindcss.setup({
 	on_attach = on_attach,
-  capabilities = capabilites,
-}
+	capabilities = capabilites,
+})
 
 -- javascript typescirpt (eslint and tsserver)
-require('lspconfig').eslint.setup{
+require("lspconfig").eslint.setup({
 	on_attach = on_attach,
-  capabilities = capabilites,
-}
-require('lspconfig').tsserver.setup{
+	capabilities = capabilites,
+})
+require("lspconfig").tsserver.setup({
 	on_attach = on_attach,
-  capabilities = capabilites,
-}
+	capabilities = capabilites,
+})
