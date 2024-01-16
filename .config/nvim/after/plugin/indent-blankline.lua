93d5325dc1f728f8954a46200532935d636fbf48
DILPREET1910 <dsahney1910@gmail.com>
2024-01-16 14:34:57 +0530

    remove horizontal indenttion highlight

--- .config/nvim/after/plugin/indent-blankline.lua
+++ .config/nvim/after/plugin/indent-blankline.lua
@@ -1,12 +1,16 @@
 local highlight = {
-    "grey",
+	"grey",
 }
 
-local hooks = require "ibl.hooks"
+local hooks = require("ibl.hooks")
 -- create the highlight groups in the highlight setup hook, so they are reset
 -- every time the colorscheme changes
 hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-    vim.api.nvim_set_hl(0, "grey", { fg = "#505050" })
+	vim.api.nvim_set_hl(0, "grey", { fg = "#505050" })
 end)
 
-require("ibl").setup { indent = { highlight = highlight } }
+require("ibl").setup({
+	scope = {
+		show_start = false, -- disable horizontal underline
+	},
+})
