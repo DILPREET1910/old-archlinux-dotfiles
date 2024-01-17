48931725f9fdf52654ade9f97e4df2f0703bee94
DILPREET1910 <dsahney1910@gmail.com>
2024-01-17 08:32:26 +0530

    yank to system clipboard

--- .config/nvim/lua/dilpreet/keymaps.lua
+++ .config/nvim/lua/dilpreet/keymaps.lua
@@ -4,8 +4,10 @@ vim.g.mapleader = " "
 --##############################
 -- Normal Mode keybinds
 --##############################
-vim.keymap.set("n","<leader>pv",vim.cmd.Ex) -- go back to previous Explorer (if in file)
-vim.keymap.set("n","<leader>h",':nohlsearch<CR>') -- clear search
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- go back to previous Explorer (if in file)
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>") -- clear search
+vim.keymap.set("n", "y", '"+y') -- yank to system clipboard
+vim.keymap.set("n", "yy", '"+yy') -- yank to system clipboard
 
 --##############################
 -- Insert Mode keybinds
