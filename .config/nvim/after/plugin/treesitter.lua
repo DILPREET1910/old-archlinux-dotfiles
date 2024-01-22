0388896cc697aa64640a2c7692ab7f29edf87809
DILPREET1910 <dsahney1910@gmail.com>
2024-01-22 16:31:46 +0530

    tabsapce/indentation to 2 -> fixes the treesitter autopair fuckup

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -15,5 +15,5 @@ require("nvim-treesitter.configs").setup({
 		-- Instead of true it can also be a list of languages
 		additional_vim_regex_highlighting = false,
 	},
-	indent = { enable = false },
+	indent = { enable = true },
 })
--- .config/nvim/lua/dilpreet/options.lua
+++ .config/nvim/lua/dilpreet/options.lua
@@ -3,8 +3,8 @@ vim.opt.nu = true
 vim.opt.relativenumber = true
 
 -- tabspcae
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.shiftround = true
 vim.opt.expandtab = true
 
