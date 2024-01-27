c176a88ef6c4c1304c700d3dd9bed8a77efec9e6
DILPREET1910 <dsahney1910@gmail.com>
2024-01-27 14:10:00 +0530

    fixed bashls formating

--- .config/nvim/after/plugin/nonels.lua
+++ .config/nvim/after/plugin/nonels.lua
@@ -6,7 +6,6 @@ null_ls.setup({
 		null_ls.builtins.formatting.prettier,
 		null_ls.builtins.diagnostics.eslint,
 		null_ls.builtins.diagnostics.shellcheck,
-		null_ls.builtins.formatting.bashls,
 	},
 })
 
