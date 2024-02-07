2c1dc6d35adc14f56756347246e58e359be55b8d
DILPREET1910 <dsahney1910@gmail.com>
2024-02-07 19:22:36 +0530

    cpu frequency instead of percentage

--- .config/suckless/slstatus/config.h
+++ .config/suckless/slstatus/config.h
@@ -65,7 +65,7 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{cpu_perc," [ CPU:%s%% ]",NULL},
+	{cpu_freq," [ CPU:%s ]",NULL},
 	{ram_used,"   [ RAM:%s/",NULL},
 	{ram_total,"%s ]",NULL},
 	{disk_used,"   [ DISK:%s/","/home"},
