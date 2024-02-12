0c9eb74ef316394b83b6b920aa7a961fc9a50098
DILPREET1910 <dsahney1910@gmail.com>
2024-02-12 16:25:14 +0530

    shell check code actions

--- .config/nvim/after/plugin/nonels.lua
+++ .config/nvim/after/plugin/nonels.lua
@@ -4,11 +4,18 @@ null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.prettier,
-		null_ls.builtins.diagnostics.eslint, -- javascript and typescript linter
-		null_ls.builtins.diagnostics.shellcheck, -- bash script linter
+
+		-- javascript and typescript linter
+		null_ls.builtins.diagnostics.eslint,
+
+		-- bash script linter
+		null_ls.builtins.diagnostics.shellcheck,
+		null_ls.builtins.code_actions.shellcheck,
+
+		-- java linter (check style)
 		null_ls.builtins.diagnostics.checkstyle.with({
 			extra_args = { "-c", "/google_checks.xml" },
-		}), -- java linter (check style)
+		}),
 	},
 })
 
