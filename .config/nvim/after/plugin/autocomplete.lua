9bd57553e86ce30f9aeb39c03fc33fdf6173c97b
DILPREET1910 <dsahney1910@gmail.com>
2024-02-21 21:39:33 +0530

    autocomplete keymap to select next prev recommendation

--- .config/nvim/after/plugin/autocomplete.lua
+++ .config/nvim/after/plugin/autocomplete.lua
@@ -8,8 +8,8 @@ cmp.setup({
 		documentation = cmp.config.window.bordered(),
 	},
 	mapping = cmp.mapping.preset.insert({
-		["<C-b>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-b>"] = cmp.mapping.select_next_item(),
+		["<C-f>"] = cmp.mapping.select_prev_item(),
 		["<C-o>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.abort(),
 		["<CR>"] = cmp.mapping.confirm({ select = true }),
