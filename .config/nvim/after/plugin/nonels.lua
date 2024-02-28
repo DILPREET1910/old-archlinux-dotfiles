784a5b329ac3fc62de04c77c5197feecbe82fd76
DILPREET1910 <dsahney1910@gmail.com>
2024-02-28 11:01:09 +0530

    none ls eslint shell check support depricated

--- .config/nvim/after/plugin/nonels.lua
+++ .config/nvim/after/plugin/nonels.lua
@@ -5,13 +5,6 @@ null_ls.setup({
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.prettier,
 
-		-- javascript and typescript linter
-		null_ls.builtins.diagnostics.eslint,
-
-		-- bash script linter
-		null_ls.builtins.diagnostics.shellcheck,
-		null_ls.builtins.code_actions.shellcheck,
-
 		-- java linter (check style)
 		null_ls.builtins.diagnostics.checkstyle.with({
 			extra_args = { "-c", "/google_checks.xml" },
