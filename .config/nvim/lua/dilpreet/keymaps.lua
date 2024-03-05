05a46924878bc25cb05775e55af2ddd31ce512f9
DILPREET1910 <dsahney1910@gmail.com>
2024-03-05 14:05:44 +0530

    keymaps for vertical and horizontal split

--- .config/nvim/lua/dilpreet/keymaps.lua
+++ .config/nvim/lua/dilpreet/keymaps.lua
@@ -10,6 +10,8 @@ vim.keymap.set("n", "y", '"+y') -- yank to system clipboard
 vim.keymap.set("n", "yy", '"+yy') -- yank to system clipboard
 vim.keymap.set("n", "<leader>q", "@q") -- execute macro
 vim.keymap.set("n", "<leader>p", "\"_dP") -- don't let delete fuck up your registory
+vim.keymap.set("n", "<leader>sv", ":vs<CR>") -- vertical split
+vim.keymap.set("n", "<leader>sh", ":sp<CR>") -- horizontal split
 
 --##############################
 -- Insert Mode keybinds
